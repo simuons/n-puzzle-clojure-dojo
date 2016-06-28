5459e4941212a273364cc73326e6b8d0e69c3318
Simonas Pinevičius <simonasp@wix.com>
2016-06-28 14:41:21 +0300

    Convert to parameterized test

--- test/n_puzzle/core_test.clj
+++ test/n_puzzle/core_test.clj
@@ -4,10 +4,12 @@
 
 (deftest board-test
   (testing "create a board of a given enge size"
-    (is (= (board 0) []))
-    (is (= (board 1) []))
-    (is (= (board 2) [[1 2]
-                      [3 nil]]))
-    (is (= (board 3) [[1 2 3]
-                      [4 5 6]
-                      [7 8 nil]]))))
+    (are [edge-size expected-board]
+      (= (board edge-size) expected-board)
+      0 []
+      1 []
+      2 [[1 2]
+         [3 nil]]
+      3 [[1 2 3]
+         [4 5 6]
+         [7 8 nil]])))
